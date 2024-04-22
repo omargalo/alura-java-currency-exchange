8a7746492815a044e89c8925e7987da31744e772
Omar García López <omargarcialopez81@outlook.com>
2024-04-22 08:03:03 -0600

    add CurrencyResponse Record (DTO)

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -2,18 +2,15 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.Gson;
 
 public class CurrencyConverter {
     private static final String API_KEY = "584238861b12151d4c59f2f2";
     private static final String BASE_URL = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/pair/";
+    private final Gson gson = new Gson();
 
     public double convertCurrency(String fromCurrency, String toCurrency, double amount) {
-        String url = BASE_URL + fromCurrency + "/" + toCurrency;
-        if (amount > 0) {
-            url += "/" + amount; // Añadir el monto a la URL si es mayor que 0
-        }
+        String url = BASE_URL + fromCurrency + "/" + toCurrency + (amount > 0 ? "/" + amount : "");
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
@@ -21,20 +18,19 @@ public class CurrencyConverter {
 
         try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            JsonObject jsonResponse = JsonParser.parseString(response.body()).getAsJsonObject();
+            CurrencyResponse currencyResponse = gson.fromJson(response.body(), CurrencyResponse.class);
 
-            if (jsonResponse.get("result").getAsString().equals("success")) {
+            if ("success".equals(currencyResponse.result())) {
                 if (amount > 0) {
                     // Si se especificó un monto, devolver el resultado de la conversión
-                    return jsonResponse.get("conversion_result").getAsDouble();
+                    return currencyResponse.conversion_result() != null ? currencyResponse.conversion_result() : 0;
                 } else {
                     // Si no se especificó monto, devolver solo la tasa de cambio
-                    return jsonResponse.get("conversion_rate").getAsDouble();
+                    return currencyResponse.conversion_rate();
                 }
             } else {
                 // Manejar errores según el tipo
-                String errorType = jsonResponse.get("error-type").getAsString();
-                System.err.println("Error en la API: " + errorType);
+                System.err.println("Error en la API: " + currencyResponse.result());
                 return 0;
             }
         } catch (Exception e) {
